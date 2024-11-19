beb71e7f07c350ef83bcaf611e5ddfd2956e5681
magaleg <s.s.horlach@student.khai.edu>
2024-11-19 11:40:47 +0200

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -20,6 +20,14 @@ public class ChatBot {
 
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
+        System.out.println("Now I will prove to you that I can count to any number you want!");
+        Scanner scanner1 = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + "!");
+        }
     }
 }
 
