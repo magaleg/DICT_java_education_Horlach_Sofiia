876651def25b81096cb800edfa683ffd703f82b8
magaleg <s.s.horlach@student.khai.edu>
2024-11-19 16:21:08 +0200

    Hangman stage 5th

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,27 +1,55 @@
 package Hangman;
 
-import java.util.Random;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] words = {"python", "java", "javascript", "kotlin"};
+        String wordToGuess = words[(int) (Math.random() * words.length)];
 
-        Random random = new Random();
-        String randomWord = words[random.nextInt(words.length)];
-
-        String hint = randomWord.substring(0, 2) + "-".repeat(randomWord.length() - 2);
-
-        System.out.println("HANGMAN");
-        System.out.println("Guess the word " + hint + ": > ");
-
-        String guess = scanner.nextLine();
-
-        if (guess.equals(randomWord)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+        HashSet<Character> guessedLetters = new HashSet<>();
+        char[] displayWord = new char[wordToGuess.length()];
+        for (int i = 0; i < displayWord.length; i++) {
+            displayWord[i] = '-';
         }
+
+        int attemptsLeft = 8;
+        System.out.println("HANGMAN");
+
+        while (attemptsLeft > 0) {
+            System.out.println(String.valueOf(displayWord));
+            System.out.print("Input a letter: > ");
+            char guess = scanner.next().charAt(0);
+
+            if (guessedLetters.contains(guess)) {
+                System.out.println("You've already guessed this letter");
+                continue;
+            }
+
+            guessedLetters.add(guess);
+            boolean correctGuess = false;
+
+            for (int i = 0; i < wordToGuess.length(); i++) {
+                if (wordToGuess.charAt(i) == guess) {
+                    displayWord[i] = guess;
+                    correctGuess = true;
+                }
+            }
+
+            if (!correctGuess) {
+                System.out.println("That letter doesn't appear in the word");
+                attemptsLeft--;
+            }
+
+            if (String.valueOf(displayWord).equals(wordToGuess)) {
+                System.out.println("You guessed the word " + wordToGuess + "!");
+                System.out.println("You survived!");
+                return;
+            }
+        }
+
+        System.out.println("You lost!");
     }
 }
